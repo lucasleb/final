e1cc65a9c7d1361f505a8944e22e463ff5f72d02
Lucas Lebrun <lucasleb@msn.com>
2015-03-27 09:34:35 -0400

    nameuser

--- Gemfile
+++ Gemfile
@@ -14,7 +14,8 @@ gem 'bootstrap-sass'
 gem 'paperclip', '~> 4.2'
 gem 'aws-sdk', '< 2.0'
 gem 'masonry-rails',  '~> 0.2.4'
-
+gem 'will_paginate', '~> 3.0.5'
+gem 'will_paginate-bootstrap'
 
 gem 'sdoc', '~> 0.4.0', group: :doc
 
--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -4,8 +4,8 @@ class PinsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   def index
-    @pins = Pin.all
-  end
+   @pins = Pin.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 1)
+ end
 
   def show
   end
